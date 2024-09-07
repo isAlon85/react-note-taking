5a65689b99e088d6809172924099d78b40dbcae4
Israel Alonso <ianstry85@gmail.com>
2024-09-07 20:19:44 +0200

    routes done

---
+++ src/NoteForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Form, Stack } from 'react-bootstrap';
+
+export default function NoteForm() {
+  return (
+    <Form>
+      <Stack gap={4}></Stack>
+    </Form>
+  );
+}
