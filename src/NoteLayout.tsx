c532325a7afba92023b9100263e26f04acd0c451
Israel Alonso <93823993+isAlon85@users.noreply.github.com>
2024-09-09 22:42:55 +0200

    creation done with staged changes

---
+++ src/NoteLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NoteLayout(): JSX.Element {
+  return (
+    <div style={{ backgroundColor: '#f2f2f2', padding: '20px' }}>
+      <h1>Note Layout</h1>
+    </div>
+  )
+}
